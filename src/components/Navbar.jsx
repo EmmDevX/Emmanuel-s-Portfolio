762f41393fd4928a306dd67fd9d70dbd15d317a2
EmmDevX <elevatedtechnology2023@gmail.com>
2026-04-24 10:32:07 +0100

    Init Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,9 @@ function Navbar() {
         <li><a href="#home" onClick={() => setIsMenuOpen(false)}>Home</a></li>
         <li><a href="#about" onClick={() => setIsMenuOpen(false)}>About Me</a></li>
         <li><a href="#projects" onClick={() => setIsMenuOpen(false)}>Projects</a></li>
+                <li><a href="#skills" onClick={() => setIsMenuOpen(false)}>Skills</a></li>
         <li><a href="#contact" onClick={() => setIsMenuOpen(false)}>Contact</a></li>
+        
         <li className="mobile-talk-item">
           <a href="https://wa.me/+2347025183733" className="lets-talk-btn" onClick={() => setIsMenuOpen(false)}>
             Let's Talk
